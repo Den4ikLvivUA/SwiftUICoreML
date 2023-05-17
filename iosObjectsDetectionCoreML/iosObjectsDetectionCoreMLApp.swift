de4916aa8e9f9412379f861efee4533ca5c764d8
Denys Havryliak <den4iklvivua@gmail.com>
2023-05-17 17:13:54 +0300

    Added template

--- iosObjectsDetectionCoreML/iosObjectsDetectionCoreMLApp.swift
+++ iosObjectsDetectionCoreML/iosObjectsDetectionCoreMLApp.swift
@@ -10,11 +10,26 @@ import SwiftUI
 @main
 struct iosObjectsDetectionCoreMLApp: App {
     let persistenceController = PersistenceController.shared
-
+    
+    init() {
+        UINavigationBar.applyCustomAppearance()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CameraView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+fileprivate extension UINavigationBar {
+    
+    static func applyCustomAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
